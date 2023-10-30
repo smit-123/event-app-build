fdb4f014968128c60ac71e9d37011c0b3384aca8
Smit Suthar <smit@goswirl.live>
2023-10-30 12:19:52 +0530

    first commit

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const port = process.env.PORT || 3000;
 // Serve static files from the 'build' folder
 app.use(express.static(path.join(__dirname, 'build')));
 
-// Define a route that serves the React app
-app.get('/', (req, res) => {
+// Define a route that serves the React app for all routes except APIs
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
